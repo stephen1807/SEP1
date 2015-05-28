0a123e0f9bba1dabf75e32eb11e3e5648cc2c2b5
ekadenyaz <yayaz_33@rocketmail.com>
2015-05-28 15:56:33 +0800

    Added Servlets

---
+++ src/proj/servlet/LogoutServlet.java
@@ -0,0 +1,28 @@
+package proj.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by Stephen on 2015/05/15.
+ */
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        session.removeAttribute("userid");
+
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
---
+++ src/proj/servlet/QueryLogServlet.java
@@ -0,0 +1,36 @@
+package proj.servlet;
+
+import proj.obj.AttendanceLog;
+import proj.service.AttendanceLogService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Yayaz_33 on 5/28/2015.
+ */
+@WebServlet(name = "QueryLogServlet")
+public class QueryLogServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int employeeID = Integer.parseInt(request.getParameter("employeeID"));
+
+        AttendanceLogService service = AttendanceLogService.getInstance();
+
+        List<AttendanceLog> tempList = service.getAttendanceLog(employeeID);
+
+        request.setAttribute("attendanceList", tempList);
+
+        request.getRequestDispatcher("/EditSuccess.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        this.doPost(request, response);
+    }
+}
