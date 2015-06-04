254ec3367407575e0e391a6d8fe4ac0e2fdaa9cd
stephen1807 <stef.stephen.r@gmail.com>
2015-06-04 18:45:39 +0800

    Added functions for inactivity tracking

---
+++ src/proj/logic/AttendanceLogLogic.java
@@ -0,0 +1,19 @@
+package proj.logic;
+
+import java.util.Date;
+
+/**
+ * Created by Stephen on 2015/06/04.
+ */
+public class AttendanceLogLogic {
+
+    public void userSetInactive(int employeeID, Date entryDate) {
+
+
+    }
+
+    public void userSetActive(int employeeID, Date entryDate) {
+
+
+    }
+}
