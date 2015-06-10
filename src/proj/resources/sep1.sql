6a5c5a047857be691efeada5d4e4c144150c9ae5
stephen1807 <stef.stephen.r@gmail.com>
2015-06-10 22:03:06 +0800

    Exported SQL database (cherry picked from commit 0c4e0d6)

---
+++ src/proj/resources/sep1.sql
@@ -0,0 +1,214 @@
+CREATE DATABASE  IF NOT EXISTS `sep1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sep1`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: sep1
+-- ------------------------------------------------------
+-- Server version	5.6.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `attendancelog`
+--
+
+DROP TABLE IF EXISTS `attendancelog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attendancelog` (
+  `logid` int(11) NOT NULL AUTO_INCREMENT,
+  `employeeid` int(11) NOT NULL,
+  `entrydate` datetime NOT NULL,
+  `logcode` int(11) NOT NULL,
+  PRIMARY KEY (`logid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendancelog`
+--
+
+LOCK TABLES `attendancelog` WRITE;
+/*!40000 ALTER TABLE `attendancelog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendancelog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `employeeid` int(11) NOT NULL AUTO_INCREMENT,
+  `employeetype` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `userpass` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `departmentID` int(11) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `dob` date NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `basesalary` float NOT NULL,
+  PRIMARY KEY (`employeeid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,1,'admin','admin','Admin',0,'m','1999-07-18',123,20000);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logcode`
+--
+
+DROP TABLE IF EXISTS `logcode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logcode` (
+  `logcode` int(11) NOT NULL,
+  `logname` varchar(100) NOT NULL,
+  `logdescription` varchar(2000) DEFAULT NULL,
+  `logcost` float NOT NULL,
+  `logunit` varchar(7) NOT NULL,
+  `logcost_permission` float NOT NULL,
+  PRIMARY KEY (`logcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logcode`
+--
+
+LOCK TABLES `logcode` WRITE;
+/*!40000 ALTER TABLE `logcode` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logcode` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permission` (
+  `permissionid` int(11) NOT NULL AUTO_INCREMENT,
+  `logcode` int(11) NOT NULL,
+  PRIMARY KEY (`permissionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissionglobal`
+--
+
+DROP TABLE IF EXISTS `permissionglobal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissionglobal` (
+  `permissionid` int(11) NOT NULL,
+  `repeatdays` varchar(7) NOT NULL,
+  `starthour` int(11) NOT NULL,
+  `startminute` int(11) NOT NULL,
+  `endhour` int(11) NOT NULL,
+  `endminute` int(11) NOT NULL,
+  PRIMARY KEY (`permissionid`),
+  CONSTRAINT `permissionglobal_ibfk_1` FOREIGN KEY (`permissionid`) REFERENCES `permission` (`permissionid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissionglobal`
+--
+
+LOCK TABLES `permissionglobal` WRITE;
+/*!40000 ALTER TABLE `permissionglobal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissionglobal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissionindividual`
+--
+
+DROP TABLE IF EXISTS `permissionindividual`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissionindividual` (
+  `permissionid` int(11) NOT NULL,
+  `employeeid` int(11) NOT NULL,
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL,
+  PRIMARY KEY (`permissionid`),
+  CONSTRAINT `permissionindividual_ibfk_1` FOREIGN KEY (`permissionid`) REFERENCES `permission` (`permissionid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissionindividual`
+--
+
+LOCK TABLES `permissionindividual` WRITE;
+/*!40000 ALTER TABLE `permissionindividual` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissionindividual` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `relation`
+--
+
+DROP TABLE IF EXISTS `relation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `relation` (
+  `relationid` int(11) NOT NULL AUTO_INCREMENT,
+  `superiorid` int(11) NOT NULL,
+  `subordinateid` int(11) NOT NULL,
+  PRIMARY KEY (`relationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relation`
+--
+
+LOCK TABLES `relation` WRITE;
+/*!40000 ALTER TABLE `relation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `relation` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-06-10 22:02:14
